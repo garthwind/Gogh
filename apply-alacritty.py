e823f210b4cff2616ba5d48fbaf7e596da755dbe
garthwind <garthwind@pm.me>
2022-04-03 12:04:27 -0400

    Fix apply-alacritty.py for sparse config
    Instead of always attempting an update on a key that is assumed to
    exist, now updates only when the key is present and inserts when it
    isn't.

--- apply-alacritty.py
+++ apply-alacritty.py
@@ -4,14 +4,17 @@ import sys
 from appdirs import user_config_dir
 from ruamel.yaml import YAML # use ruamel.yaml to preserve comments in config
 
-
 yaml = YAML()
 conf = user_config_dir('alacritty') + "/alacritty.yml"
+print(conf)
 # Read alacritty config
 with open(conf, 'r') as stream:
     data_loaded = yaml.load(stream)
-
+    breakpoint()
+print(data_loaded)
+# TODO - Add IndexError handling
 js = json.loads(sys.argv[1])
+print(js)
 
 # Use update to not remove existing comments
 data_loaded['colors']['primary'].update(js['colors']['primary'])
